74cd1e414ffb7fa09dcab809f42afea8255b8b7d
Shawn Jacobsen <5thkey1@gmail.com>
2025-06-07 12:21:12 -0400

    Day 1: Added Random JavaScript Exercise from Udacity

---
+++ Random JS quizes.js
@@ -0,0 +1,53 @@
+/*
+ * Programming Quiz: First Expression
+ * Write an expression that uses at least three, distinct, arithmetic 
+ * operators to log the number 42 to the console.
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators.
+ *    (`+`, `-`, `*`, `/`, or `%`) 
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log(1 + 5 - 2);
+
+
+console.log(8 * 5 + 2 / 1);
+// console.log((1 + 5 - 2) * 10 + 2 );
+// console.log((1 + 5 - 2) * 10 + 2 / 1 );
+// console.log(10 * 4 + 4 - 2);
+// console.log(80 - 40 + 2 / 1)
+
+
+/*
+ * Programming Quiz: Converting Temperatures
+ *
+ * The Celsius-to-Fahrenheit formula:
+ *
+ *    F = C x 1.8 + 32
+ *
+ * 1. Set the fahrenheit variable to the correct value using the
+ *    celsius variable and the formula above
+ * 2. Log the fahrenheit variable to the console
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `celsius`
+ * 2. You code should have a variable `fahrenheit`
+ * 3. Your variable `celsius` should equal `12`
+ * 4. Your variable `fahrenheit` should produce the output equal `53.6`
+ * 5. Your variable `fahrenheit` declaration should use the `celsius` variable
+ * 6. Your variable `fahrenheit` should have the correct formula
+ * 7. Your code should log the `fahrenheit` variable
+ */
+ 
+
+const celsius = 12;
+const fahrenheit = 12 * 1.8 + 32 
+
+console.log(fahrenheit);
