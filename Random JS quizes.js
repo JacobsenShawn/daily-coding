7d958ac5cf7fe846b1393bb1e1a662a5923879cd
Shawn <5thkey1@gmail.com>
2025-06-09 15:04:37 -0400

    Adding Ice cream quiz

--- Random JS quizes.js
+++ Random JS quizes.js
@@ -193,3 +193,63 @@ console.log("Thank you. Have a nice day!");
 // Customer8	35	false	false	Thank you. Have a nice day!
 // Customer9	0	false	false	Thank you. Have a nice day!
 // Customer10	0	true	false	Your account is no longer active.
+
+// |----------------------------------------------------------------------------------|
+
+/*
+ * Programming Quiz: Ice Cream
+ *
+ * Write a single if statement that logs out the message:
+ * 
+ * "Great choice! Your ice cream is at the next window."
+ * 
+ * ...only if:
+ *   - flavor is "vanilla" or "chocolate"
+ *   - vessel is "sugar cone" or "wafer cone"
+ *   - topping is "sprinkles" or "peanuts"
+ *
+ * Otherwise log out:
+ *
+ * "Please check our menu and try again."
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `flavor`, `vessel`, and `topping`
+ * 2. Your code should have an `if` statement
+ * 3. Your code should use logical expressions
+ * 
+ */
+ 
+ 
+//  Testing Your Code
+
+// How will you know if your code works? Change the values of flavor, topping and vessel and re-run the code and compare your output with the expected output.
+
+
+
+//  Assigning variables 
+const flavor = "vanilla";
+const topping = "sprinkles";
+const vessel = "wafer cone";
+
+
+
+if ((flavor === "vanilla" || flavor === "chocolate") && (topping === "sprinkles" || topping === "peanuts") && (vessel === "sugar cone" || vessel === "wafer cone")){
+    console.log ("Great choice! Your ice cream is at the next window.");
+} else {
+    console.log ("Please check our menu and try again.");
+}
+
+// Expected Output
+// Flavor	Topping	Vessel	Response
+// chocolate	bananas	wafer cone	"Please check our menu and try again."
+// chocolate	peanuts	wafer cone	"Great choice! Your ice cream is at the next window."
+// chocolate	sprinkles	sugar cone	"Great choice! Your ice cream is at the next window."
+// chocolate	sprinkles	bowl	"Please check our menu and try again."
+// strawberry	sprinkles	wafer cone	"Please check our menu and try again."
+// strawberry	bananas	sugar cone	"Please check our menu and try again."
+// strawberry	peanuts	bowl	"Please check our menu and try again."
+// vanilla	sprinkles	wafer cone	"Great choice! Your ice cream is at the next window."
+// vanilla	peanuts	sugar cone	"Great choice! Your ice cream is at the next window."
+// vanilla	sprinkles	bowl	"Please check our menu and try again."
