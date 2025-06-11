deb2641b1e12c843a96538bf02956abc82722c0a
Shawn <5thkey1@gmail.com>
2025-06-11 11:28:56 -0400

    Adding Fizzbuzz like version

--- Random JS quizes.js
+++ Random JS quizes.js
@@ -427,4 +427,49 @@ console.log("In 2015, a person with "+education+" earned an average of $"+salary
 
 
 
-// |--------------------------------------------------------------------------------\
+// |----------------------------------------------------------------------------|
+
+/*
+ * Programming Quiz: JuliaJames
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `x` with a starting value of `1`
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should use a conditional statement
+ * - Your code should increment `x` by `1` each time the loop executes
+ * - Your code should produce the expected output
+ */
+// Directions:
+
+// Write a while loop that:
+
+//     Loop through the numbers 1 to 20
+//     If the number is divisible by 3, print "Julia"
+//     If the number is divisible by 5, print "James"
+//     If the number is divisible by 3 and 5, print "JuliaJames"
+//     If the number is not divisible by 3 or 5, print the number
+
+
+//  have to do this one backwards becasue if it includes a 3 or a 5 it would print that 
+// before making it to 3 and 5 , so start at 3and 5 , then 5 , then 3 , then number 
+let x = 1;
+
+// while loop with a stop condition
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("JuliaJames");
+    }else if (x % 5 === 0) {
+        console.log("James");
+    }else if (x % 3 === 0) {
+        console.log("Julia");
+    }
+    else {
+        console.log(x);
+    }
+    // increment x at the end of each loop
+    x = x + 1 ; 
+}
+
+// |----------------------------------------------------------------------------|
