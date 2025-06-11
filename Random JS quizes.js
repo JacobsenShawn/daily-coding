08d9c066a84aaa5b6517666e0962439e970aa35b
Shawn <5thkey1@gmail.com>
2025-06-11 11:47:17 -0400

    99 Bottles  Quiz

--- Random JS quizes.js
+++ Random JS quizes.js
@@ -473,3 +473,40 @@ while (x <= 20) {
 }
 
 // |----------------------------------------------------------------------------|
+
+/*
+ * Programming Quiz: 99 Bottles of Juice
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `num` with a starting value of `99`
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output
+ */
+
+
+let num = 99;
+
+while (num >= 1) {
+    if (num  === 1) {
+        console.log(`${num} bottle of juice on the wall! ${num} bottle of juice! Take one down, pass it around... ${num - 1} bottles of juice on the wall!`);
+    // Second-last iteration. Note occurances of bottleS, bottleS, bottle
+    } else if (num === 2){
+        console.log(`${num} bottles of juice on the wall! ${num} bottles of juice! Take one down, pass it around... ${num - 1} bottle of juice on the wall!`);
+    } else {
+        // When there's only 1 bottle left
+        console.log(`${num} bottle of juice on the wall! ${num} bottle of juice! Take one down, pass it around... no more bottles of juice on the wall!`);
+    }
+    num = num - 1; // decrement num
+}
+
+// |----------------------------------------------------------------------------|
